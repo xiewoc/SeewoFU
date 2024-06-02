28fb8a87bcfeb4074f7b7a5ce05c00ac21cb62dc
xiewoc <70128845+xiewoc@users.noreply.github.com>
2024-06-02 14:39:18 +0800

    Update Resource.h

--- Resource.h
+++ Resource.h
@@ -1,6 +1,9 @@
-#define IDI_NOTIFICATIONICON        207
-//#define IDB_BITMAP1    130
-#define IDM_EXIT       101
-#define IDM_ABOUT      102
-#define IDM_FULL       103
-#define IDM_MIN        104
+#define IDI_NOTIFICATIONICON			207
+#define IDB_BG							211
+#define IDM_EXIT       					101
+#define IDM_ABOUT      					102
+#define IDM_LOCKAGAIN					103
+#define IDR_PAUSE 						12
+
+#define WM_SHOWTASK (WM_USER+114)
+#define VK_CLICKED(VK_KEY)((GetAsyncKeyState(VK_KEY) & 0x8000) != 0) 
