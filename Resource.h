cf51a243560b3389dd9a53742e5114a75cd487d7
xiewoc <70128845+xiewoc@users.noreply.github.com>
2024-10-13 04:26:26 +0800

    Delete Resource.h

--- Resource.h
+++
@@ -1,9 +0,0 @@
-#define IDI_NOTIFICATIONICON			207
-#define IDB_BG							211
-#define IDM_EXIT       					101
-#define IDM_ABOUT      					102
-#define IDM_LOCKAGAIN					103
-#define IDR_PAUSE 						12
-
-#define WM_SHOWTASK (WM_USER+114)
-#define VK_CLICKED(VK_KEY)((GetAsyncKeyState(VK_KEY) & 0x8000) != 0) 
