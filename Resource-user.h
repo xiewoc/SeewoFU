e8b5247acbb56ae0a103ff070cad997cd3f43ab1
xiewoc <70128845+xiewoc@users.noreply.github.com>
2024-10-13 04:27:05 +0800

    Add files via upload

---
+++ Resource-user.h
@@ -0,0 +1,22 @@
+#define IDI_NOTIFICATIONICON			207
+#define IDB_BG							211
+#define IDB_BG1							212
+#define IDW_WAVE1						311
+#define IDM_EXIT       					101
+#define IDM_ABOUT      					102
+#define IDM_LOCKAGAIN					103
+//#define IDM_PLAYSOUND					104
+//#define IDM_DESTORYBLOCKSHUTDOWN		105 
+#define IDM_LOGCATSWICH					106
+#define IDM_FAKESDMSG					107
+#define IDR_PAUSE 						12
+#define IDC_BUTTON						1001 
+#define IDC_BUTTON1						1002 
+#define LOG_ERR							1501
+#define LOG_INFO						1502
+#define LOG_CATON						1503
+#define LOG_CATOFF						1504
+
+
+#define WM_SHOWTASK (WM_USER+114)
+#define VK_CLICKED(VK_KEY)((GetAsyncKeyState(VK_KEY) & 0x8000) != 0) 
